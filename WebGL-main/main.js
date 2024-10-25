059d0ae0f1ffb1529372028c5805ed6f29bf9a3e
Михаил Кизириди <shadowyas9@gmail.com>
2024-10-25 13:35:25 +0300

    Lab1. Applied wireframe rendering to get a clear visualisation of the shape without complex shaders. Adjusted CreateSurfaceData() to create an index list for drawing lines. Updated drawing mode from gl.TRIANGLES to gl.LINES in Model.Draw().

--- WebGL-main/main.js
+++ WebGL-main/main.js
@@ -5,70 +5,45 @@ let surface;                    // A surface model
 let shProgram;                  // A shader program
 let spaceball;                  // A SimpleRotator object that lets the user rotate the view by mouse.
 
+
 function deg2rad(angle) {
     return angle * Math.PI / 180;
 }
 
 
-// Constructor
-// function Model(name) {
-//     this.name = name;
-//     this.iVertexBuffer = gl.createBuffer();
-//     this.count = 0;
-
-//     this.BufferData = function(vertices) {
-
-//         gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
-//         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STREAM_DRAW);
-
-//         this.count = vertices.length/3;
-//     }
-
-//     this.Draw = function() {
-
-//         gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
-//         gl.vertexAttribPointer(shProgram.iAttribVertex, 3, gl.FLOAT, false, 0, 0);
-//         gl.enableVertexAttribArray(shProgram.iAttribVertex);
-   
-//         gl.drawArrays(gl.LINE_STRIP, 0, this.count);
-//     }
-// }
 class Model {
     constructor(name) {
         this.name = name;
         this.vertexBuffer = gl.createBuffer();
-        this.indexBuffer = gl.createBuffer();
-        this.indexCount = 0;
+        this.lineIndexBuffer = gl.createBuffer();
+        this.vertexCount = 0;
+        this.lineIndexCount = 0;
     }
 
-    // Buffer both vertex and index data
-    BufferData(data) {
-        let vertices = data.vertexList;
-        let indices = data.indexList;
-
+    BufferData(surfaceData) {
         // Bind and buffer vertex data
         gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+        gl.bufferData(gl.ARRAY_BUFFER, surfaceData.vertices, gl.STATIC_DRAW);
+        this.vertexCount = surfaceData.vertices.length / 3;
 
-        // Bind and buffer index data
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
-        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
-
-        // Store the count of indices
-        this.indexCount = indices.length;
+        // Bind and buffer line index data for wireframe
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.lineIndexBuffer);
+        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, surfaceData.lineIndices, gl.STATIC_DRAW);
+        this.lineIndexCount = surfaceData.lineIndices.length;
     }
 
-    // Draw method using index buffer
     Draw() {
         gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer);
         gl.vertexAttribPointer(shProgram.iAttribVertex, 3, gl.FLOAT, false, 0, 0);
         gl.enableVertexAttribArray(shProgram.iAttribVertex);
 
-        // Bind the index buffer and draw elements
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
-        gl.drawElements(gl.TRIANGLES, this.indexCount, gl.UNSIGNED_SHORT, 0);
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.lineIndexBuffer);
+
+        // Draw wireframe with LINES
+        gl.drawElements(gl.LINES, this.lineIndexCount, gl.UNSIGNED_SHORT, 0);
     }
 }
+
 
 // Constructor
 function ShaderProgram(name, program) {
@@ -94,105 +69,86 @@ function ShaderProgram(name, program) {
  * way to draw with WebGL.  Here, the geometry is so simple that it doesn't matter.)
  */
 function draw() { 
-    gl.clearColor(0,0,0,1);
+    gl.clearColor(0.9, 0.9, 0.9, 1);  // Light gray background for contrast
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    
-    /* Set the values of the projection transformation */
-    let projection = m4.perspective(Math.PI/8, 1, 8, 12); 
-    
-    /* Get the view matrix from the SimpleRotator object.*/
+
+    // Set up projection and modelView matrices as before
+    let projection = m4.perspective(Math.PI / 4, 1, 0.1, 100);
     let modelView = spaceball.getViewMatrix();
+    let translateToPointZero = m4.translation(0, 0, -10); 
+    let matAccum0 = m4.multiply(translateToPointZero, modelView);
+    let modelViewProjection = m4.multiply(projection, matAccum0);
 
-    let rotateToPointZero = m4.axisRotation([0.707,0.707,0], 0.7);
-    let translateToPointZero = m4.translation(0,0,-10);
+    // Send transformation matrix to shader
+    gl.uniformMatrix4fv(shProgram.iModelViewProjectionMatrix, false, modelViewProjection);
 
-    let matAccum0 = m4.multiply(rotateToPointZero, modelView );
-    let matAccum1 = m4.multiply(translateToPointZero, matAccum0 );
-        
-    /* Multiply the projection matrix times the modelview matrix to give the
-       combined transformation matrix, and send that to the shader program. */
-    let modelViewProjection = m4.multiply(projection, matAccum1 );
-
-    gl.uniformMatrix4fv(shProgram.iModelViewProjectionMatrix, false, modelViewProjection );
+    // Set the color to black for lines
+    gl.uniform4fv(shProgram.iColor, [0, 0, 0, 1]);
     
-    /* Draw the six faces of a cube, with different colors. */
-    gl.uniform4fv(shProgram.iColor, [1,1,0,1] );
-
+    // Draw the wireframe model
     surface.Draw();
 }
 
+
 // V 11: Twice Oblique Trochoid Cylindroid
-function CreateSurfaceData(H, c, alpha, phi, p, uMin, uMax, vMin, vMax, nu, nv) {
-    let vertexList = [];
-    let indexList = [];
+function CreateSurfaceData(c, H, alpha, phi, k, theta0, uMin, uMax, vMin, vMax, nu, nv) {
+    let vertices = [];
+    // let normalList = [];
+    let lineIndices = [];
 
-    let du = (uMax - uMin) / nu;
-    let dv = (vMax - vMin) / nv;
+    let p = k * Math.PI;
 
-    for (let i = 0; i <= nu; i++) {
+    let du = (uMax - uMin) / (nu - 1);
+    let dv = (vMax - vMin) / (nv - 1);
+
+    for (let i = 0; i < nu; i++) {
         let u = uMin + i * du;
-        let theta = p * u;
-
-        for (let j = 0; j <= nv; j++) {
+        for (let j = 0; j < nv; j++) {
             let v = vMin + j * dv;
+
+            let theta = theta0 + p * u;
+
+            // Calculate x, y, z positions (as before)
             let x = c * u + v * (Math.sin(phi) + Math.tan(alpha) * Math.cos(phi) * Math.cos(theta));
             let y = v * Math.tan(alpha) * Math.sin(theta);
             let z = H + v * (Math.tan(alpha) * Math.sin(phi) * Math.cos(theta) - Math.cos(phi));
 
-            vertexList.push(x, y, z);
+            vertices.push(x, y, z);
 
-            // Create the index list (assuming a quad mesh)
-            if (i < nu && j < nv) {
-                let p1 = i * (nv + 1) + j;
-                let p2 = p1 + 1;
-                let p3 = p1 + (nv + 1);
-                let p4 = p3 + 1;
-
-                // First triangle
-                indexList.push(p1, p3, p2);
-                // Second triangle
-                indexList.push(p2, p3, p4);
-            }
+            // Calculate line indices for wireframe (adjacent vertices in grid)
+            if (i < nu - 1) lineIndices.push(i * nv + j, (i + 1) * nv + j);
+            if (j < nv - 1) lineIndices.push(i * nv + j, i * nv + j + 1);
         }
     }
 
-    return {
-        vertexList: new Float32Array(vertexList),
-        indexList: new Uint16Array(indexList)
-    };
+    return { vertices: new Float32Array(vertices), lineIndices: new Uint16Array(lineIndices) };
 }
 
-/* Initialize the WebGL context. Called from init() */
-// function initGL() {
-//     let prog = createProgram( gl, vertexShaderSource, fragmentShaderSource );
 
-//     shProgram = new ShaderProgram('Basic', prog);
-//     shProgram.Use();
-
-//     shProgram.iAttribVertex              = gl.getAttribLocation(prog, "vertex");
-//     shProgram.iModelViewProjectionMatrix = gl.getUniformLocation(prog, "ModelViewProjectionMatrix");
-//     shProgram.iColor                     = gl.getUniformLocation(prog, "color");
-
-//     surface = new Model('Surface');
-//     surface.BufferData(CreateSurfaceData());
-//     gl.enable(gl.DEPTH_TEST);
-// }
 function initGL() {
     let prog = createProgram(gl, vertexShaderSource, fragmentShaderSource);
 
-    shProgram = new ShaderProgram('Basic', prog);
+    shProgram = new ShaderProgram('PhongLighting', prog);
     shProgram.Use();
 
+    // Get attribute and uniform locations
     shProgram.iAttribVertex = gl.getAttribLocation(prog, "vertex");
+    shProgram.iAttribNormal = gl.getAttribLocation(prog, "normal");
     shProgram.iModelViewProjectionMatrix = gl.getUniformLocation(prog, "ModelViewProjectionMatrix");
+    shProgram.iModelViewMatrix = gl.getUniformLocation(prog, "ModelViewMatrix");
+    shProgram.iNormalMatrix = gl.getUniformLocation(prog, "NormalMatrix");
     shProgram.iColor = gl.getUniformLocation(prog, "color");
+    shProgram.iLightPosition = gl.getUniformLocation(prog, "lightPosition");
 
     surface = new Model('Surface');
 
-    // Pass both vertex and index data
-    let surfaceData = CreateSurfaceData(1, 5, 0.033 * Math.PI, 0, 8 * Math.PI, 0, 1, -5, 5, 50, 50);
-    surface.BufferData(surfaceData);
+    // Trochoid from Page 97 Fig. 2 of "EncyclopediaOfAnalyticalSurfaces.pdf"
+    // let surfaceData = CreateSurfaceData(5, 1, 0.033 * Math.PI, 0, 8, 0, 0, 1, -5, 5, 50, 50);
 
+    // Trochoid from Page 98 Fig. 3 of "EncyclopediaOfAnalyticalSurfaces.pdf"
+    let surfaceData = CreateSurfaceData(2, 1, 0.05 * Math.PI, 0, 2, 0, 0, 1, -4, 4, 50, 50);
+
+    surface.BufferData(surfaceData);
     gl.enable(gl.DEPTH_TEST);
 }
 
